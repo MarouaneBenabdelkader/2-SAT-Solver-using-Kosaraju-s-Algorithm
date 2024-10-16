85b66b333682f71d64806693e5398c3cd9b59e6c
imad <imad.cost@outlook.fr>
2024-10-16 22:53:43 +0200

    Customize Main class to handle file input from terminal for testing.

--- src/Main.java
+++ src/Main.java
@@ -6,75 +6,81 @@ public class Main {
     public static void main(String[] args) {
 //    * Test of kosaraju and DFS works (needs some modifications)
 //         Create the graph
+//        try {
+//            // Create a graph with 9 vertices
+//            Graph<String> graph1 = new Graph<>();
+//
+//            // Add arcs to the graph
+//            graph1.addArc(0, 1, "a");
+//            graph1.addArc(1, 0, "b");
+//            graph1.addArc(1, 2, "b");
+//            graph1.addArc(0, 2, "c");
+//            graph1.addArc(4, 3, "d");
+//            graph1.addArc(3, 4, "e");
+//            graph1.addArc(-2, -4, "g");
+//            graph1.addArc(-3, -4, "h");
+//            graph1.addArc(-3, -2, "i");
+//            graph1.addArc(-4, -3, "j");
+//
+//            // Print the graph for visual inspection
+//            System.out.println("Original Graph:");
+//            System.out.println(graph1);
+//
+//            // Create an instance of Kosaraju with the graph
+//            Kosaraju<String> kosaraju = new Kosaraju<>(graph1);
+//
+//            // Run Kosaraju's algorithm to get the SCCs
+//            List<Set<Integer>> SCCs = kosaraju.findStronglyConnectedComponents();
+//            TwoSatSolver twoSatSolver = new TwoSatSolver(graph1, 5);
+//            boolean satisfiable = twoSatSolver.checkSatisfiability();
+//
+//            // Output the SCCs
+//            System.out.println("\nStrongly Connected Components (SCCs):");
+//            for (Set<Integer> scc : SCCs) {
+//                System.out.println(scc);
+//            }
+//            if (satisfiable) {
+//                System.out.println("Formula " + ": satisfiable");
+//            } else {
+//                System.out.println("Formula " + ": unsatisfiable");
+//            }
+//
+//
+//        } catch (Exception e) {
+//            System.out.println("An error occurred: " + e.getMessage());
+//        }
+
+
+//        ? test of parser current state
         try {
-            // Create a graph with 9 vertices
-            Graph<String> graph1 = new Graph<>();
+            String filename = "formulas/formule-2-sat.txt"; // Default testing file
 
-            // Add arcs to the graph
-            graph1.addArc(0, 1, "a");
-            graph1.addArc(1, 0, "b");
-            graph1.addArc(1, 2, "b");
-            graph1.addArc(0, 2, "c");
-            graph1.addArc(4, 3, "d");
-            graph1.addArc(3, 4, "e");
-            graph1.addArc(-2, -4, "g");
-            graph1.addArc(-3, -4, "h");
-            graph1.addArc(-3, -2, "i");
-            graph1.addArc(-4, -3, "j");
+            // pick another file to test by passinf arguments to the Main
+            if (0 < args.length) {
+                filename = "../formulas/testSet" + args[0] + "/formula" + args[1] + ".txt";
+            }
 
-            // Print the graph for visual inspection
-            System.out.println("Original Graph:");
-            System.out.println(graph1);
+            Parser cnfParser = new CNFParser();
 
-            // Create an instance of Kosaraju with the graph
-            Kosaraju<String> kosaraju = new Kosaraju<>(graph1);
+            cnfParser.parse(filename);
+            Graph<String> graph = cnfParser.getImplicationGraph();
 
-            // Run Kosaraju's algorithm to get the SCCs
+            Kosaraju<String> kosaraju = new Kosaraju<>(graph);
             List<Set<Integer>> SCCs = kosaraju.findStronglyConnectedComponents();
-            TwoSatSolver twoSatSolver = new TwoSatSolver(graph1, 5);
+
+            TwoSatSolver twoSatSolver = new TwoSatSolver(graph, cnfParser.getNumVariables());
+
             boolean satisfiable = twoSatSolver.checkSatisfiability();
-
-            // Output the SCCs
-            System.out.println("\nStrongly Connected Components (SCCs):");
-            for (Set<Integer> scc : SCCs) {
-                System.out.println(scc);
-            }
             if (satisfiable) {
-                System.out.println("Formula " + ": satisfiable");
+                System.out.println("Formula " + filename + ": satisfiable");
             } else {
-                System.out.println("Formula " + ": unsatisfiable");
+                System.out.println("Formula " + filename + ": unsatisfiable");
             }
-
+//            }
 
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
         }
-
-
-
-
-
-//        ? test of parser current state
-//        String filename = "formulas/formule-2-sat.txt";
-//        Parser cnfParser = new CNFParser();
-//        cnfParser.parse(filename);
-//        Graph<String> graph = cnfParser.getImplicationGraph();
-//
-//        // Print the graph
-//        System.out.println(graph);
-//        Kosaraju<String> kosaraju = new Kosaraju<>(graph);
-//        List<Set<Integer>> SCCs = kosaraju.findStronglyConnectedComponents();
-//        System.out.println("\nStrongly Connected Components (SCCs):");
-//        for (Set<Integer> scc : SCCs) {
-//            System.out.println(scc);
-//        }
-//        TwoSatSolver twoSatSolver = new TwoSatSolver(graph, cnfParser.getNumVariables());
-//        boolean satisfiable = twoSatSolver.checkSatisfiability();
-//        if (satisfiable) {
-//            System.out.println("Formula " + filename + ": satisfiable");
-//        } else {
-//            System.out.println("Formula " + filename + ": unsatisfiable");
-//        }
 //        ! given code from tp initial state
 //        Parser parser = new Parser();
 //        Graph<String> graph = parser.parse(filename);
@@ -92,5 +98,4 @@ public class Main {
 //        exit(0);
 
     }
-
 }
